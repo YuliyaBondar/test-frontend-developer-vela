b9dfa37ca5881e0109033f886e2cca5c2d7fa0b6
Yulia Bondar <yuliabondar98@gmail.com>
2025-07-14 00:00:02 +0300

    feat: add mobile version for header

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const languages = document.querySelector('.header__language');
+const phone = document.querySelector('.top-block__phone');
+const hunter = document.querySelector('.interactions__hunter');
+const burger = document.querySelector('.burger');
+const headerRight = document.querySelector('.header__item_right');
+const search = document.querySelector('.search');
+const searchButton = document.querySelector('.search__button');
+
+if (window.innerWidth < 1100) {
+  phone.before(languages);
+  burger.after(hunter);
+  headerRight.after(search);
+  searchButton.textContent = '';
+}
